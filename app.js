8fc9977979c86a26ae67d2d4af104ccf96a10e83
Leo Ling <leocabbage2008@gmail.com>
2022-02-05 20:50:01 -0500

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+module.exports = app;
